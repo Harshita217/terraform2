d0b3094e23bff8c939ba5affdc5675eeb7936abe
harshita gupta <harshitagupta217@gmail.com>
2025-02-20 15:47:39 +0530

    new Change

--- main.tf
+++
@@ -1,8 +0,0 @@
-provider "azurerm" {
- subscription_id = "79c65e59-aadb-45ab-9a43-3e10d9e5225e"
-    features {}
-}
-resource "azurerm_resource_group" "rg1" {
-  name     = "rg1"
-  location = "westus"
-}
