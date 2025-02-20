d79dd26cab08930adbaf3e9d38b9bf132d40c4e0
harshita gupta <harshitagupta217@gmail.com>
2025-02-20 15:31:05 +0530

    second Change

---
+++ terra-cloud.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+ subscription_id = "79c65e59-aadb-45ab-9a43-3e10d9e5225e"
+    features {}
+}
+resource "azurerm_resource_group" "rg1" {
+  name     = "harshita-rg1"
+  location = "westus"
+}
